e1e67209899cbe585ade59617f9c635550b64063
Abby-Smith <108291503+Abby-Smith@users.noreply.github.com>
2022-11-19 14:16:49 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,3 +1,17 @@
+# Copyright 2022 The TensorFlow Authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+    # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This opens an object detector that can detect and classify common objects in live-feed video.
 
 # Before running in the terminal, folllow TensorFlow's YouTube steps:
